db80bb90477c6c92d99d1accc91d8b27bdfc8c96
Oriana E. Martínez <martinez.oriana.e@gmail.com>
2023-04-10 16:38:43 -0400

    [UPDATE] Edit Profile

---
+++ public/js/profile/edit.js
@@ -0,0 +1,173 @@
+const d = document;
+
+/* Responsive Navbar */
+
+const toggle = d.querySelector('.navbar-toggle');
+const navbar = d.querySelector('#navbar');
+
+const toggleNavbar = () => {
+    if (navbar.classList.contains('active')) {
+        navbar.classList.remove('active');
+        toggle.innerHTML = '<img id="open" src="./img/menu-open-icon.svg" alt="Menú">';
+    }
+    else {
+        navbar.classList.add('active');
+        toggle.innerHTML = '<img id="close" src="./img/menu-close-icon.svg" alt="Menú">';
+    }
+}
+
+toggle.addEventListener('click', toggleNavbar, false);
+
+/* Toggle Password */
+
+const inputPass = d.querySelector('#input-password');
+const inputPassCon = d.querySelector('#input-password-confirmation');
+const togglePass = d.querySelector('#toggle-password');
+const togglePassCon = d.querySelector('#toggle-password-confirmation');
+
+const togglePassword = () => {
+    if (inputPass.getAttribute('type') === 'password') {
+        inputPass.setAttribute('type', 'text');
+        inputPassCon.setAttribute('type', 'text');
+        togglePass.innerHTML = '<span class="icon-eye-blocked"></span>';
+        togglePassCon.innerHTML = '<span class="icon-eye-blocked"></span>';
+    }
+    else {
+        inputPass.setAttribute('type', 'password');
+        inputPassCon.setAttribute('type', 'password');
+        togglePass.innerHTML = '<span class="icon-eye"></span>';
+        togglePassCon.innerHTML = '<span class="icon-eye"></span>';
+    }
+}
+
+togglePass.addEventListener('click', togglePassword, false);
+togglePassCon.addEventListener('click', togglePassword, false);
+
+/* Validations (Update Info Profile) */
+
+const profileForm = d.querySelector('#update-profile-form');
+const errorProfile = d.querySelector('#div-error-profile');
+const inputName = d.querySelector('#input-name');
+const inputLastname = d.querySelector('#input-lastname');
+const inputAddress = d.querySelector('#input-address');
+const inputEmail = d.querySelector('#input-email');
+const buttonProfile = d.querySelector('#update-profile-button');
+
+// Regular Expression
+const regexName = /^([A-ZÁÉÍÓÚÑ]{1}[a-záéíóúñ]+[ ]?){1,2}$/;
+const regexEmail = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+
+const validName = (e) => {
+    errorProfile.innerHTML = '';
+
+    if (e.target.value.length === 0) {
+        errorProfile.innerHTML = '<span class="error-message">El campo Nombre es requerido</span>';
+    }
+    else if (!e.target.value.match(regexName)) {
+        errorProfile.innerHTML = '<span class="info-message">Ingresa un nombre válido, con la primera letra mayúscula y sin números o caracteres especiales</span>';
+    }
+}
+const validLastname = (e) => {
+    errorProfile.innerHTML = '';
+
+    if (e.target.value.length === 0) {
+        errorProfile.innerHTML = '<span class="error-message">El campo Apellido es requerido</span>';
+    }
+    else if (!e.target.value.match(regexName)) {
+        errorProfile.innerHTML = '<span class="info-message">Ingresa un apellido válido, con la primera letra mayúscula y sin números o caracteres especiales</span>';
+    }
+}
+const validAddress = (e) => {
+    errorProfile.innerHTML = '';
+
+    if (e.target.value.length === 0) {
+        errorProfile.innerHTML = '<span class="error-message">El campo Dirección es requerido</span>';
+    } 
+    else if (e.target.value.lenght > 255) {
+        errorProfile.innerHTML = '<span class="info-message">La dirección no debe sobrepasar los 255 caracteres</span>';
+    }
+}
+const validEmail = (e) => {
+    errorProfile.innerHTML = '';
+
+    if (e.target.value.length === 0) {
+        errorProfile.innerHTML = '<span class="error-message">El campo Correo Electrónico es requerido</span>';
+    }
+    else if (!e.target.value.match(regexEmail)) {
+        errorProfile.innerHTML = '<span class="info-message">Ingresa un correo electrónico válido, que incluya un @ y un .</span>';
+    }
+}
+
+const submitProfileForm = (e) => {
+    e.preventDefault();
+
+    if (
+        inputName.value.length === 0 || !inputName.value.match(regexName) || 
+        inputLastname.value.length === 0 || !inputLastname.value.match(regexName) ||
+        inputAddress.value.length === 0 || inputAddress.value.lenght > 255 ||
+        inputEmail.value.length === 0 || !inputEmail.value.match(regexEmail)
+    ) {
+        errorProfile.innerHTML = '<span class="error-message">Completa correctamente todos los campos</span>';
+    }
+    else {
+        profileForm.submit();
+    }
+}
+
+inputName.addEventListener('change', (e) => validName(e), false);
+inputLastname.addEventListener('change', (e) => validLastname(e), false);
+inputAddress.addEventListener('change', (e) => validAddress(e), false);
+inputEmail.addEventListener('change', (e) => validEmail(e), false);
+buttonProfile.addEventListener('click', (e) => submitProfileForm(e), false);
+
+/* Validations (Update Password Profile) */
+
+const passForm = d.querySelector('#update-password-form');
+const errorPass = d.querySelector('#div-error-password');
+const buttonPass = d.querySelector('#update-password-button');
+
+// Regular Expression
+const regexPass = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/;
+
+const validPassword = (e) => {
+    errorPass.innerHTML = '';
+
+    if (e.target.value.length === 0) {
+        errorPass.innerHTML = '<span class="error-message">El campo Contraseña es requerido</span>';
+    }
+    else if (!e.target.value.match(regexPass)) {
+        errorPass.innerHTML = '<span class="info-message">La contraseña debe tener entre 8 y 16 caracteres con al menos un dígito, una minúscula y una mayúscula</span>';
+    }
+}
+const validPasswordCon = (e) => {
+    errorPass.innerHTML = '';
+
+    if (inputPass.value.length === 0) {
+        errorPass.innerHTML = '<span class="info-message">Ingresa primero una nueva contraseña para confirmar</span>';
+    }
+    else {
+        if (e.target.value.length === 0) {
+            errorPass.innerHTML = '<span class="error-message">El campo Confirmación es requerido</span>';
+        }
+        else if (e.target.value !== inputPass.value) {
+            errorPass.innerHTML = '<span class="info-message">La confirmación debe coincidir con la contraseña</span>'
+        }
+    }
+}
+const submitPassForm = (e) => {
+    e.preventDefault();
+
+    if (
+        inputPass.value.length === 0 || !inputPass.value.match(regexPass) ||
+        inputPassCon.value.length === 0 || inputPass.value !== inputPassCon.value
+    ) {
+        errorPass.innerHTML = '<span class="error-message">Completa correctamente todos los campos para registrarte</span>';
+    }
+    else {
+        passForm.submit();
+    }
+}
+
+inputPass.addEventListener('change', (e) => validPassword(e), false);
+inputPassCon.addEventListener('change', (e) => validPasswordCon(e), false);
+buttonPass.addEventListener('click', (e) => submitPassForm(e), false);
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,233 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="icon" type="image/ico" href="./img/Favicon.ico" />
+        <link rel="stylesheet" href="./css/index.css" />
+        <link rel="stylesheet" href="./css/profile/edit.css">
+        <link rel="stylesheet" href="./css/styles.css" />
+
+        <title>ReyRey Sports</title>
+    </head>
+
+    <body>
+        <!-- Header (Start) -->
+        <header>
+            <!-- Navbar -->
+            <nav>  
+                <ul id="navbar">
+                    <!-- Logo -->
+                    <li id="navbar-logo">
+                        <a href="/">
+                            <img src="./img/logo-main-without-bg.svg" alt="Logo de ReyRey Sports">
+                        </a>
+                    </li>
+
+                    <!-- Separators -->
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    
+                    @if (Route::has('login'))
+                        <!-- Actions Buttons -->
+                        <li id="navbar-buttons">
+                            @auth
+                                <a href="{{ url('/dashboard') }}">
+                                    <button class="navbar-button-icon">
+                                        @if (Auth::user()->rol_id === 1)
+                                            <span class="icon-dashboard"></span>
+                                        @else            
+                                            <span class="icon-shopping-cart"></span>
+                                        @endif
+                                    </button> 
+                                </a>
+
+                                <a href="{{ url('/profile') }}">
+                                    <button class="navbar-button-icon">
+                                        <span class="icon-user"></span>
+                                    </button>    
+                                </a>
+
+                                <form id="navbar-form" method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    
+                                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.querySelector('#navbar-form').submit();">
+                                        <button class="navbar-button">Logout</button>        
+                                    </a>
+                                </form>
+                            @else
+                                <a href="{{ route('login') }}" class="navbar-button">
+                                    <button class="navbar-button">Login</button>        
+                                </a>
+
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}">
+                                        <button class="navbar-button">Register</button>
+                                    </a>
+                                @endif
+                            @endauth
+                        </li>
+                    @endif      
+                    
+                    <!-- Toggle Menu -->
+                    <li class="navbar-toggle">
+                        <img id="open" src="./img/menu-open-icon.svg" alt="Menú">
+                    </li>
+                </ul>                   
+            </nav>
+        </header>
+        <!-- Header (End) -->
+
+        <!-- Body (Start) -->
+        <main>
+            <!-- Update Info Profile -->
+            <section id="update-profile">
+                <div class="header-section">
+                    <h2> INFORMACIÓN DEL PERFIL</h2>
+
+                    <p>Actualiza tu información personal y correo electrónico</p>
                 </div>
+
+                <!-- Error Message -->
+                <div id="div-error-profile">
+                    @if ($errors->any())
+                        @foreach($errors->all() as $error)
+                            @if ($error === "The email has already been taken.")
+                                <p class="error-message">El correo electrónico ya esta registrado</p>
+                            @else
+                                <p class="error-message">{{ $error }}</p>
+                            @endif
+                        @endforeach
+                    @endif
+                </div>
+
+                <form id="update-profile-form" method="POST" action="{{ route('profile.update') }}">
+                    @csrf
+                    @method('patch')
+
+                    <div id="div-update-profile-form">
+                        <x-text-input id="input-name" name="name" type="text" :value="old('name', $user->name)" placeholder="Nombre" />
+                        <x-text-input id="input-lastname" name="last_name" type="text" :value="old('last_name', $user->last_name)" placeholder="Apellido" />
+                        <x-text-input id="input-address" name="address" type="text" :value="old('address', $user->address)" placeholder="Dirección" />
+                        <x-text-input id="input-email" name="email" type="text" :value="old('email', $user->email)" placeholder="Correo Electrónico" />
+                    </div>
+                    
+                    <x-primary-button id="update-profile-button">{{ __('Actualizar') }}</x-primary-button>
+                </form>
+
+                <!-- Success Message -->
+                <div id="div-success-profile">
+                    @if (session('status') === 'profile-updated')
+                        <span class="success-message">
+                            <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)">
+                                {{ __('Datos Actualizados con Éxito') }}
+                            </p>
+                        </span>
+                    @endif
+                </div>
+            </section>
+
+            <!-- Update Password Profile -->
+            <section id="update-password">
+                <div class="header-section">
+                    <h2>GESTIÓN DE CONTRASEÑA</h2>
+
+                    <p>Actualiza tu contraseña para una mayor seguridad</p>
+                </div>
+
+                <!-- Error Message -->
+                <div id="div-error-password">
+                    @if ($errors->any())
+                        @foreach($errors->all() as $error)
+                            <p class="error-message">{{ $error }}</p>
+                        @endforeach
+                    @endif
+                </div>
+
+                <form id="update-password-form" method="POST" action="{{ route('password.update') }}">
+                    @csrf
+                    @method('put')
+                    
+                    <div id="div-update-password-form">
+                        <x-text-input id="input-current-password" name="current_password" type="password" placeholder="Contraseña Actual" />
+                        
+                        <div class="password">
+                            <x-text-input id="input-password" name="password" type="password"  placeholder="Nueva Contraseña" />
+
+                            <button type="button" id="toggle-password">
+                                <span class="icon-eye"></span>
+                            </button>
+                        </div>
+                        <div class="password">
+                            <x-text-input id="input-password-confirmation" name="password_confirmation" type="password" placeholder="Confirmación"  />
+                            
+                            <button type="button" id="toggle-password-confirmation">
+                                <span class="icon-eye"></span>
+                            </button>
+                        </div>
+                    </div>
+
+                    <x-primary-button id="update-password-button">{{ __('Actualizar') }}</x-primary-button>
+                </form>
+
+                <!-- Success Message -->
+
+                <div id="div-success-password">
+                    @if (session('status') === 'password-updated')
+                        <span class="success-message">
+                            <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)">
+                                {{ __('Contraseña Actualizada con Éxito') }}
+                            </p>
+                        </span>
+                    @endif
+                </div>
+            </section>
+        </main>
+        <!-- Body (End) -->
+
+        <!-- Footer (Start) -->
+        <footer>
+            <!-- Social Media -->
+            <div id="footer-social-media">
+                <a class="footer-social-media-icon" href="https://www.facebook.com/" target="_blank">
+                    <span class="icon-facebook2"></span>
+                </a>
+                <a class="footer-social-media-icon" href="https://www.instagram.com/" target="_blank">
+                    <span class="icon-instagram"></span>
+                </a>
+                <a class="footer-social-media-icon" href="https://twitter.com/?lang=es" target="_blank">
+                    <span class="icon-twitter"></span>
+                </a>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
+            <div id="footer-texts">
+                <!-- Links -->
+                <ul id="footer-links">
+                    <li class="footer-link">
+                        <a href="#">Términos de Uso</a>
+                    </li>
+                    <li class="footer-link">
+                        <a href="#">Políticas de Privacidad</a>
+                    </li>
+                    <li class="footer-link">
+                        <a href="#">Contáctanos</a>
+                    </li>
+                </ul>
+
+                <p id="footer-copyright">Copyright &#169; 2023, ReyRey Sports</p>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+            <!-- Logo -->
+            <img id="footer-logo" src="./img/logo-second-without-bg.svg" alt="Logo Simple de ReyRey Sports">
+        </footer>
+        <!-- Footer (End) -->
+
+        <!-- Scripts -->
+        <script src="./js/profile/edit.js"></script>
+    </body>
+</html>
