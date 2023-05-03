7f6b9766fc05947e2048821534566f4a97fc9cc3
reyl27 <reyfellugo@gmail.com>
2023-05-03 18:15:20 -0400

    cambios nuevos
    se agrego el contenido de terminos y politica

--- resources/views/extras/policies.blade.php
+++ resources/views/extras/policies.blade.php
@@ -96,10 +96,136 @@
             </nav>
         </header>
         <!-- Header (End) -->
-        
+
         <!-- Body -->
         <main>
-            <h2>Pagina de Políticas de Privacidad</h2>
+               <!--mini cabecera  -->
+            <div class="privacy-policy">
+                <div class="common-policy contenido">
+                    <div class="cont-encabezado">
+                        <div class="espacio izq"></div>
+                        <div class="centro">
+                         <div class="contenido-centro">Política de Privacidad</div>
+                        </div>
+                        <div class="espacio derch"></div>
+                    </div >
+                    <!-- contenido <div ></div> <h2></h2> <b></b>  <p></p>-->
+                    <div class="Cuerpo">
+                        <div >
+                            <h2>Política de Privacidad</h2>
+                            <p>
+                                <b>Fecha de entrada en vigor: 3 de mayo de 2023</b>
+                            </p>
+                            <p>
+                            Esta Política de Privacidad describe cómo ReyRey sports ("ReyRey sports", "nosotros", "nuestro", “nos”) recopila y procesa datos personales sobre usted ("usted" o "su"), cómo usamos y protegemos esos datos, y sus derechos en relación con ellos cuando utiliza el sitio web <b>poner link</b> (o su sucesor) y cualquier servicio relacionado (el/los “Servicio(s)”).
+                            </p>
+                            <p>El Servicio es un programa de fidelización gratificante que le permite comprar artículos y prendas entre usted y la tienda afiliada que figura en el Servicio.
+                            </p>
+                            <p>Esta Política de Privacidad se aplica a todos los datos personales que recopilamos o procesamos sobre usted. Los datos personales son cualquier información sobre usted, así como una combinación de información que pueda permitir razonablemente que usted o su hogar sean identificados.</p>
+                        </div>
+                        <div >
+                            <h2>Los Datos que Recogemos</h2>
+                            <p>Recogemos y almacenamos ciertos datos como resultado de sus interacciones con nuestro Servicio:</p>
+                            <ul>
+                                <li>
+                                    "Nombre;"
+                                </li>
+                                <li>
+                                    "Su ID de inicio de sesión y contraseña;"
+                                </li>
+                                <li>
+                                    "Su dirección de correo electrónico;"
+                                </li>
+                                <li>
+                                    "Información sobre sus datos de interacción, comunicación y conectividad;"
+                                </li>
+                                <li>
+
+                                    "La dirección de protocolo de Internet (IP) utilizada para conectar su computadora o dispositivo móvil a Internet;"
+                                </li>
+                                <li>
+                                    "Su computadora o dispositivo móvil e información de conexión, como el tipo y versión de su navegador, su sistema operativo y plataforma;"
+                                </li>
+                                <li>
+                                    "El flujo de clics completo de los Localizadores Uniformes de Recursos (URL) hacia, a través, y desde el Servicio correspondiente (incluida la fecha y la hora)"
+                                </li>
+                                <li>
+                                    "Los parámetros UTM de la campañia;"
+                                </li>
+                                <li>
+                                    "Información sobre su actividad en Internet u otras redes electrónicas, como información sobre su comportamiento de navegación en línea, preferencias e historial de compras. Parte de esta información la recopilamos automáticamente de usted, por ejemplo, los datos recogidos mediante cookies y otras tecnologías de identificación de dispositivos ("Cookies y tecnologías de seguimiento");"
+                                </li>
+                                <li>
+                                    "La segmentación de su usuario para personalizar su experiencia (en boletines y ofertas en el sitio web);"
+                                </li>
+                                <li>
+                                    "Si utiliza la funcionalidad de nuestra barra de herramientas, su UUID de miembro (en una cookie);"
+                                </li>
+                            </ul>
+                            <p>Puede encontrar más información sobre nuestro uso de cookies y tecnologías de seguimiento en nuestra Política de Cookies.</p>
+                            <p>Es su responsabilidad actualizar y mantener información de contacto precisa en su cuenta de reyRey sports. Puede realizar cambios visitando la página de su cuenta e introduciendo las correcciones necesarias.</p>
+                            <p>Tenga en cuenta que podemos estar obligados, por ley, a recopilar determinados datos personales sobre usted, o como consecuencia de cualquier relación contractual que tengamos con usted. El hecho de no proporcionar esta información puede impedir o retrasar el cumplimiento de estas obligaciones. Le informaremos, en el momento de la recogida de sus datos, de la obligatoriedad de determinados datos y de las consecuencias de no facilitarlos.</p>
+                            <p></p>
+                        </div>
+                        <div >
+                            <h2>CÓMO UTILIZAMOS SUS DATOS</h2>
+                            <p>Recopilamos y almacenamos estos datos como resultado de sus interacciones con nuestros Servicios, para los siguientes fines:</p>
+                            <p><b>Identificación y autenticación: </b> utilizamos sus identificadores para verificar su identidad cuando accede a su cuenta de ReyRey sports ID y para garantizar la seguridad de sus datos personales. El ID de ReyRey sports también le permite utilizar otros servicios proporcionados por otras empresas del Grupo ReyRey sports (como se explica más adelante);</p>
+                            <p><b> Prestación del Servicio:</b> analizamos la información sobre cómo usa el Servicio para ofrecer una mejor experiencia a todos los usuarios y mejorar los servicios de Rakuten, y para el desarrollo del negocio;</p>
+                            <p><b>Mejora del Servicio: </b> analizamos la información sobre cómo usa el Servicio para ofrecer una mejor experiencia a todos los usuarios y mejorar los servicios de Rakuten, y para el desarrollo del negocio;</p>
+                            <p><b>Personalización de su experiencia y recomendaciones:</b>  cuando utiliza el Servicio, podemos utilizar sus datos personales para mejorar su experiencia, por ejemplo, proporcionando elementos interactivos o personalizados y brindándole recomendaciones basadas en sus intereses;</p>
+                            <p><b>Comunicarnos con usted:</b> podemos usar sus datos personales para comunicarnos con usted, por ejemplo, si le proporcionamos información sobre cambios en los términos y condiciones o si se pone en contacto con nosotros para hacernos preguntas;</p>
+                            <p><b>Marketing:</b> podemos utilizar sus datos personales para crear un perfil sobre usted y ubicarlo en determinados segmentos de marketing, con el fin de comprender mejor sus preferencias y personalizar adecuadamente los mensajes de marketing que le enviamos. También podemos usar sus identificadores para enviarle comunicaciones de marketing sobre el Servicio y otros productos o servicios que puedan ser de su interés;</p>
+                            <p><b>Ejercicio de nuestros derechos:</b> podemos usar sus datos personales para ejercer nuestros derechos legales cuando sea necesario, por ejemplo, para detectar, prevenir y responder a reclamaciones por fraude, reclamaciones por infracción de la propiedad intelectual o violaciones de la ley o de nuestros términos y condiciones</p>
+                            <p><b> Cumplir con nuestras obligaciones:</b> podemos tratar sus datos personales para cumplir con los requisitos legales o reglamentarios, cuando la ley así lo exija explícitamente, como en respuesta a órdenes judiciales o para cumplir con regulaciones financieras o de privacidad;</p>
+                            <p><b>Protección contra daños:</b> Debemos tener una base legal para procesar sus datos personales. En la mayoría de los casos, la base legal será una de las siguientes:</p>
+                            <p>Debemos tener una base legal para procesar sus datos personales. En la mayoría de los casos, la base legal será una de las siguientes: </p>
+                            <ol>
+                            <li>Para cumplir con nuestras obligaciones contractuales con usted, por ejemplo, para prestar los Servicios y para garantizar que el reembolso se otorgue correctamente. No proporcionar esta información puede impedir o retrasar el cumplimiento de estas obligaciones contractuales;</li>
+                            <li>Para cumplir con nuestras obligaciones legales;</li>
+                            <li>Para satisfacer nuestros intereses legítimos, por ejemplo, para comprender cómo utiliza nuestros productos y servicios y permitirnos obtener conocimiento de ello, lo que a su vez nos permite desarrollar nuevos productos y servicios. Cuando procesamos datos personales para satisfacer nuestros intereses legítimos, establecemos sólidas medidas de seguridad para garantizar la protección de su privacidad y asegurar que nuestros intereses legítimos no se vean anulados por sus intereses o derechos y libertades fundamentales.</li>
+                            <li>Podemos obtener su consentimiento para recopilar y utilizar ciertos tipos de datos personales cuando la ley así nos lo exija (por ejemplo, en relación con nuestras actividades de marketing directo). Si le solicitamos su consentimiento para procesar sus datos personales, puede retirarlo en cualquier momento poniéndose en contacto con nosotros en los detalles que se encuentran al final de esta Política de Privacidad.</li>
+                            </ol>
+                        </div>
+                        <div >
+                            <h2>TRANSFERENCIAS DE SUS DATOS</h2>
+                            <p>Sus datos personales pueden ser transferidos a países que pueden no tener una protección de sus datos equivalente a la de su país de residencia, en particular a los países en los que se encuentran las empresas del Grupo ReyRey sports. Nos aseguramos de que sus derechos y libertades con respecto al tratamiento de sus datos personales estén protegidos de manera adecuada y apropiada, y para cumplir con todos los requisitos legales.</p>
+                            <p>Como parte del Grupo ReyRey sports, confiamos en las Normas corporativas vinculantes del Grupo ReyRey sports para legitimar las transferencias internacionales de datos dentro del Grupo. Para las transferencias fuera del Grupo ReyRey sports, nos basamos en las Cláusulas contractuales Estándar.</p>
+                        </div>
+                        <div >
+                            <h2>SUS DERECHOS RELACIONADOS CON SUS DATOS PERSONALES</h2>
+                            <p></p>
+                            <ul>
+                                <li>acceder a sus datos personales</li>
+                                <li>rectificar la información que tenemos sobre usted</li>
+                                <li>borrar sus datos personales</li>
+                                <li> restringir el uso de sus datos personales</li>
+                                <li>oponerse a que utilicemos sus datos personales</li>
+                                <li>recibir sus datos personales en un formato electrónico utilizable y transmitirlos a un tercero (derecho a la portabilidad de datos)</li>
+                                <li>presentar una queja ante la Autoridad local de Protección de Datos.</li>
+                            </ul>
+                            <p>Le animamos a que se ponga en contacto con nosotros para actualizar o corregir su información si ésta cambia o si los datos personales que tenemos sobre usted son inexactos.</p>
+                            <p>Puede enviarnos su consulta utilizando el formulario de contacto en <p>poner link</p> </p>
+                        </div>
+                        <div >
+                            <h2>CAMBIOS EN NUESTRA POLÍTICA DE PRIVACIDAD</h2>
+                            <p>Ocasionalmente, podemos modificar esta Política de Privacidad para dar cuenta de los cambios en nuestros Servicios y la forma en que manejamos sus datos. Si se modifica esta Política de Privacidad, se lo notificaremos con una antelación razonable. Los cambios se aplicarán a partir del momento en que éstos se le notifiquen.</p>
+                        </div>
+                        <div >
+                            <h2>Fecha de entrada en vigor: 3 de Mayo de 2023</h2>
+                            <p>Esta política de privacidad describe cómo en ReyRey Sports ("nosotros", "nuestro", "nos") recogemos y procesamos datos personales sobre usted, cómo utilizamos y protegemos esos datos, y sus derechos en relación con ellos, cuando utiliza el sitio web <b>poner link</b>   (o su sucesor) y cualquier servicio relacionado, (los «servicios»).  </p>
+                            <p>Esta Política de Privacidad se aplica a todos los datos personales que recogemos o procesamos sobre usted. Los datos personales son cualquier información sobre usted, así como una combinación de elementos de información que podrían permitir razonablemente su identificación o la de su familia.</p>
+                        </div>
+                        <div >
+                            <h2>SEGURIDAD Y CONSERVACIÓN DE DATOS</h2>
+                            <p>Aplicamos medidas técnicas y organizativas para garantizar un nivel de seguridad adecuado al riesgo de la información personal que procesamos.  Estas medidas tienen por objeto garantizar la integridad y confidencialidad permanentes de la información personal. Evaluamos estas medidas periódicamente para garantizar la seguridad del tratamiento.</p>
+                            <p>Conservaremos sus datos personales durante el tiempo necesario para los fines descritos anteriormente. Sólo conservaremos su información personal después de este tiempo si estamos obligados a hacerlo para cumplir con las leyes locales aplicables al servicio de <b>poner link</b></p>
+                        </div>
+   <!-- fin del contenido <div ></div> <h2></h2> <b></b>  <p></p>-->
+                    </div>
+
+                </div>
+            </div>
         </main>
 
         <!-- Footer (Start) -->
--- resources/views/extras/terms.blade.php
+++ resources/views/extras/terms.blade.php
@@ -96,10 +96,88 @@
             </nav>
         </header>
         <!-- Header (End) -->
-        
+
         <!-- Body -->
         <main>
-            <h2>Pagina de Términos de Uso</h2>
+           <!--mini cabecera  -->
+           <div class="privacy-policy">
+                <div class="common-policy contenido">
+                    <div class="cont-encabezado">
+                        <div class="espacio izq"></div>
+                        <div class="centro">
+                         <div class="contenido-centro">Términos y condiciones</div>
+                        </div>
+                        <div class="espacio derch"></div>
+                    </div >
+                    <!-- contenido <div ></div> <h2></h2> <b></b>  <p></p>-->
+                    <div class="Cuerpo">
+                        <div >
+                            <h2>TÉRMINOS DEL SERVICIO DE USUARIO</h2>
+                            <p>
+                                <b>Fecha de entrada en vigor: 3 de mayo de 2023</b>
+                            </p>
+                            <p>
+                            Por favor, lea lo siguiente detenidamente. Los términos siguientes forman un contrato vinculante entre usuarios de este servicio (“usuario” o “usted”) y ReyRey sports, que rige el uso de los servicios de Rakuten. Empezaremos a prestarle los Servicios de inmediato contra su aceptación del presente Acuerdo. Usted de forma expresa certifica y pacta que no podrá retirarse de este Acuerdo de un modo distinto al especificado a continuación una vez se hayan iniciado los Servicios.
+                            </p>
+                        </div>
+                        <div >
+                            <h2>CON QUIÉN TIENE EL CONTRATO</h2>
+                            <p>Este contrato (el "Acuerdo") se suscribe entre usted y ReyRey sports ("REYREY SPORTS"), inscrita en el Registro Mercantil y Societario de venezuela con el número B136664 y con RIF número j22375408. Puede ponerse en contacto con ReyRey sports en:</p>
+                            <ul>
+                                <li>
+                                    "Dirección: "
+                                </li>
+                                <li>
+                                Contacto: ""LINK de contactanos """
+                                </li>
+
+                            </ul>
+                            <p>Salvo que se indique de forma expresa, ningún tercero ostenta ningún derecho en virtud del presente Acuerdo.</p>
+
+                        </div>
+                        <div >
+                            <h2>PROPIEDAD INTELECTUAL Y OTROS DERECHOS</h2>
+                            <p>Tenga en cuenta que todo el contenido, incluyendo, entre otros, gráficos, logos, iconos, texto, imágenes, clips de audio, descargas digitales, compilaciones de datos y software publicado por ReyRey sports en o a través de los Servicios es propiedad del Grupo de Rey Rey sports o sus cedentes de licencias. ReyRey sports u otras marcas comerciales registradas y no registradas y las marcas utilizadas en relación con los Servicios son propiedad del Grupo de ReyRey sports y no podrán utilizarse sin disponer del permiso previo.</p>
+                            <p>Nosotros le otorgamos una licencia limitada, personal, no exclusiva, no transferible y revocable para acceder y realizar un uso personal y no comercial de los Servicios. Cualquier derecho no expresamente otorgado queda expresamente reservado. Sin limitación de esta reserva de derechos, esta licencia excluye de forma expresa la modificación, distribución, copiado, reproducción, nueva publicación, venta, obtención de cualquier obra derivada, extracción o utilización de partes de los Servicios; un enlace profundo a los Servicios sin contar con nuestro consentimiento expreso por escrito; cualquier trama o técnicas similares de los Servicios; el uso de cualesquiera metaetiquetas u otras etiquetas; o cualquier extracción o depuración de datos. Esta licencia queda automáticamente rescindida si usted incumple este Acuerdo.</p>
+                            <p>Se prohíbe expresamente el uso de depuración de datos, robots, rastreadores o cualquier software o herramienta similar de recopilación y extracción de datos para extraer datos de los servicios sin contar con el correspondiente consentimiento previo. </p>
+                        </div>
+                        <div >
+                            <h2>CERTIFICACIONES DE REYREY SPORTS</h2>
+                            <p>Nos comprometemos a destinar la competencia y diligencia razonables a la hora de prestarle los Servicios.</p>
+                            <p>Luchamos por garantizar que la información sobre nuestros servicios sea exacta y no escatimaremos esfuerzos para corregir los errores y omisiones tan rápidamente como sea viable después de recibir la correspondiente notificación.</p>
+                            <p>Aparte, no emitimos ninguna promesa, expresa o implícita, sobre los Servicios, que le serán prestados "tal cual". En concreto, no prometemos nada con respecto a (a) los productos que usted compre en la Tienda; (b) la exactitud de cualquier información facilitada por la Tienda o ReyRey sports a través de los Servicios; o que (c) los Servicios cumplan sus requisitos, estén siempre disponibles o carezcan de errores.</p>
+                        </div>
+                        <div >
+                            <h2>SU CUENTA</h2>
+                            <p>Al objeto de utilizar los Servicios, necesita abrir una cuenta con nosotros (su "Cuenta"). <b>Aquí poner link de registro</b> encontrará los detalles sobre cómo abrir y gestionar su Cuenta. Solamente puede tener una Cuenta con ReyRey sports.  Puede encontrar más detalles sobre ReyRey sports ID en nuestra <b> Política de Privacidad..poner link</b> </p>
+
+                            <p>Por favor, asegúrese de que la información que nos proporcione en su Cuenta sea exacta, completa y esté actualizada. Es su responsabilidad..</p>
+                            <p>También es responsable de adoptar todas las medidas razonables para salvaguardar la seguridad de su Cuenta, incluyendo la conservación de la confidencialidad de su contraseña..</p>
+                            <p>Nos comunicaremos principalmente con usted por correo electrónico. Usted acepta que se comunicará electrónicamente con nosotros. Usted está conforme con recibir cualquier comunicación nuestra electrónicamente y con que todos los acuerdos, notificaciones, publicaciones y demás comunicaciones cursadas entre nosotros electrónicamente satisfagan cualquier requisito legal y que dichas notificaciones consten por escrito. Usted asume la responsabilidad de asegurarnos que pueda recibir correos electrónicos nuestros. Cualquier comunicación nuestra estará sujeta a nuestra<b> Política de Privacidad..poner link</b> </p>
+                            <p>Nos reservamos el derecho a suspender o finalizar su Cuenta de inmediato si usted incumple el presente Acuerdo.</p>
+                            <p>Usted puede cancelar su Cuenta escribiéndonos a nuestro domicilio social o, de manera alternativa, puede contactarnos:LINK de ayuda  Atendiendo a su solicitud, nosotros cerraremos su cuenta tan pronto como sea razonablemente posible, sujeto al estado de su Cuenta.</p>
+                            <p>También nos reservamos el derecho a cancelar Cuentas que hayan permanecido inactivas durante más de doce (12) meses.</p>
+                        </div>
+                        <div >
+                            <h2>PRIVACIDAD</h2>
+                            <p>Por favor, revise nuestra "Política de Privacidad" para obtener información sobre cómo utilizamos sus datos personales. Por favor, revise nuestro Aviso sobre cookies para obtener información sobre cómo utilizamos los cookies.</p>
+                        </div>
+                        <div >
+                            <h2>CAMBIOS EN LOS SERVICIOS O DE ESTE ACUERDO</h2>
+                            <p>Al objeto de ofrecerle una experiencia y unos servicios óptimos, nuestros servicios son objeto de una constante evolución. Le informaremos con un preaviso razonable de cualquier modificación esencial de nuestros servicios. Dichos cambios no afectarán al uso previo del Servicio. Si usted está descontento con cualquier cambio que introduzcamos, es libre de suspender el uso de los Servicios. </p>
+                            <p>Ocasionalmente también tendremos la necesidad de introducir modificaciones en este Acuerdo. Le informaremos con el preaviso razonable de cualquier modificación de este Acuerdo. Dichos cambios no afectarán al uso previo de los Servicios. Si usted está descontento con los cambios que introduzcamos, es libre de poner fin a este Acuerdo.</p>
+                        </div>
+                        <div >
+                            <h2>COMUNICACIONES</h2>
+                            <p>Le enviaremos avisos y otras comunicaciones a la dirección de correo electrónico que nos haya proporcionado. También le proporcionaremos de vez en cuando información mediante un aviso en los Servicios. Cualquier comunicación nuestra estará sujeta a nuestra Política de Privacidad .</p>
+                            <p>Debe enviarnos todas las notificaciones y otras comunicaciones utilizando los datos de contacto indicados anteriormente.</p>
+                            <p>Cualquier notificación que nos envíe a través del formulario de contacto en "LINK de AYUDA y contactanos" se considerará recibida 24 horas después de la hora de envío por el remitente. Cualquier notificación enviada por correo a nosotros o a usted en la dirección que figura en su cuenta se considerará recibida al siguiente día laborable (siendo un día que no sea sábado, domingo o festivo en España, Luxemburgo o su lugar de residencia habitual). Se considerará que ha recibido las notificaciones de los Servicios la siguiente vez que utilice los Servicios y se le presente la notificación.</p>
+                        </div>
+                        <!-- fin del contenido <div ></div> <h2></h2> <b></b>  <p></p>-->
+                    </div>
+
+                </div>
+            </div>
         </main>
 
         <!-- Footer (Start) -->
